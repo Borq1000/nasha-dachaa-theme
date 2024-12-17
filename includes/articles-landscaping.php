c32c4aa344649bdf70acf7a681e32af9c792966a
Borq1000 <rodopi1000@gmail.com>
2024-12-18 02:35:59 +0300

    почти переделан полностью дизайн блока отображения статей по боагоустройству на главной.

--- includes/articles-landscaping.php
+++ includes/articles-landscaping.php
@@ -1,40 +1,47 @@
-<section style="margin-top:50px;" class="relative-articles-section descriotion">
-        <div class="container">
-
-
-
-            <div style="background: none !important" class="row">
-
-                <div class="header-h2 header-h2-left" style="margin-bottom: 20px;padding-left: 12px;">
-                    <div class="h3">Статьи на теме благоустройство территории</div>
-                </div>
-
-                <?php $latest_post = get_posts('category=3&orderby=date&numberposts=10&numberposts='); // Defaults args fetch posts starting with the most recent ?>
-                <?php foreach ($latest_post as $post) : setup_postdata($post); ?>
-
-                    <div class="col-md-4 col-sm-6">
-                        <div class="inner-fp-ablock">
-                            <a href="<?php the_permalink() ?>" alt="<?php the_title(); ?>">
-                                <img src="<?php the_post_thumbnail_url(); ?>" class="img-responsive rel-artic-img" alt="<?php the_title('', '', 0); ?>">
+<section class="latest-articles">
+    <div class="container">
+        <div class="h3">Статьи на теме благоустройство территории</div>
+        
+        <div class="row latest-articles-grid">
+            <?php $latest_post = get_posts('category=3&orderby=date&numberposts=10'); ?>
+            <?php foreach ($latest_post as $post) : setup_postdata($post); ?>
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <article class="article-card">
+                        <div class="article-image">
+                            <a href="<?php the_permalink() ?>" class="image-link">
+                                <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>" class="img-responsive">
                             </a>
-
-
-                            <h3 class="text-center rel-artic-title">
-                                <a href="<?php the_permalink() ?>" alt="<?php the_title(); ?>">
+                            <div class="article-date">
+                                <i class="far fa-calendar-alt"></i>
+                                <?php echo get_the_date('d.m.Y'); ?>
+                            </div>
+                        </div>
+                        
+                        <div class="article-content">
+                            <h3 class="article-title">
+                                <a href="<?php the_permalink() ?>">
                                     <?php
-                                    $tit = the_title('', '', 0);
-                                    echo mb_strimwidth($tit, 0, 60, "...");
+                                    $title = get_the_title();
+                                    if (mb_strlen($title) > 47) {
+                                        echo mb_substr($title, 0, 47) . '...';
+                                    } else {
+                                        echo $title;
+                                    }
                                     ?>
                                 </a>
                             </h3>
-
+                            <div class="article-excerpt">
+                                <?php echo mb_strimwidth(get_the_excerpt(), 0, 120, '...'); ?>
+                            </div>
+                            <a href="<?php the_permalink() ?>" class="read-more">
+                                Читать далее
+                                <i class="fas fa-arrow-right"></i>
+                            </a>
                         </div>
-                    </div>
-
-                <?php endforeach; ?>
-                <?php wp_reset_query(); ?>
-
-            </div>
-
+                    </article>
+                </div>
+            <?php endforeach; ?>
+            <?php wp_reset_query(); ?>
         </div>
-    </section>
+    </div>
+</section>
