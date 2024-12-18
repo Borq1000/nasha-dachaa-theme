8cd86b691a3ae1cdf84e94de47a8c78cbd9a3331
Borq1000 <rodopi1000@gmail.com>
2024-12-18 04:29:56 +0300

    немного изменил внешний вид блока с фразами

--- front-page.php
+++ front-page.php
@@ -121,26 +121,7 @@
 
 
 
-<section class="section-queries">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-7">
-                <div class="offers-queries-wraper">
-                    <div class="h3" style="color:#fff">Высокое качество работ</div>
-                    <?php require_once 'includes/predlojeniya.php';  ?>
-                </div>
-            </div>
-            
-            <div class="col-md-5">
-                <div class="queries-wraper">
-                    <div class="h3" style="color:#fff">Популярные запросы</div>
-                    
-                    <?php require_once 'includes/zaprosi.php';  ?>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
+<?php require_once 'includes/populars.php'; ?>
 
 
 
---
+++ includes/populars.php
@@ -0,0 +1,20 @@
+<section class="section-queries">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7">
+                <div class="offers-queries-wraper">
+                    <div class="h3" style="color:#fff">Высокое качество работ</div>
+                    <?php require_once 'predlojeniya.php';  ?>
+                </div>
+            </div>
+            
+            <div class="col-md-5">
+                <div class="queries-wraper">
+                    <div class="h3" style="color:#fff">Популярные запросы</div>
+                    
+                    <?php require_once 'zaprosi.php';  ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
