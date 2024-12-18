b4ba653f100c120c70f8a02bee372b5a0b047d94
Borq1000 <rodopi1000@gmail.com>
2024-12-18 16:51:46 +0300

    Улучшено отображение все проектов на странице проектов

--- archive-construction_project.php
+++ archive-construction_project.php
@@ -27,37 +27,66 @@
     </div>
 
     <div class="row" id="projects-container">
-      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <?php $related_pages = get_the_terms(get_the_ID(), 'related_services'); ?>
-        <div class="col-md-3 related-projects-col" data-term="<?php echo $related_pages[0]->slug; ?>">
-          <div class="thumbnail related-projects-thumbnail">
+      <?php 
+      $counter = 0;
+      if (have_posts()) : while (have_posts()) : the_post(); 
+        $counter++;
+        $related_pages = get_the_terms(get_the_ID(), 'related_services'); 
+      ?>
+        <div class="col-md-4 col-sm-6 related-projects-col" data-term="<?php echo $related_pages[0]->slug; ?>">
+          <div class="related-projects-thumbnail">
             <?php $main_photo = get_post_meta(get_the_ID(), 'main_photo', true); ?>
-            <?php if (!empty($main_photo)) : ?>
-              <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-              <img src="<?php echo esc_url($main_photo); ?>" class="related-projects-carousel-img" alt="<?php the_title(); ?>">
+            <div class="project-image-wrapper">
+              <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" class="project-image-link">
+                <img src="<?php echo !empty($main_photo) ? esc_url($main_photo) : 'https://nasha-dachaa.ru/wp-content/uploads/2023/04/kiparosivik.jpg'; ?>" 
+                     class="related-projects-carousel-img" 
+                     alt="<?php the_title_attribute(); ?>" />
               </a>
-          <?php else : ?>
-              <img src="https://nasha-dachaa.ru/wp-content/uploads/2023/04/kiparosivik.jpg" class="related-projects-carousel-img" alt="<?php the_title(); ?>">
-            <?php endif; ?>
-            <div class="caption related-projects-caption">
-              <div class="related-project-carousel-it-title"><?php the_title(); ?></div>
-              <?php $completion_time = get_post_meta(get_the_ID(), 'completion_time', true); ?>
+            </div>
+            
+            <div class="related-projects-caption">
+              <h3 class="related-project-carousel-it-title">
+                <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+                  <?php the_title(); ?>
+                </a>
+              </h3>
+              
               <div class="related-projects-carousel-preview-info">
-                <?php if (!empty($completion_time)) : ?>
-                  <p class="related-projects-time">Сроки: <?php echo esc_html($completion_time); ?> </p>
-                <?php endif; ?>
-                <?php $area = get_post_meta(get_the_ID(), 'area', true); ?>
-                <?php if (!empty($area)) : ?>
-                  <p class="related-projects-area">Площадь: <?php echo esc_html($area); ?> кв.м.</p>
-                <?php endif; ?>
-                <?php if ($related_pages && !is_wp_error($related_pages)) : ?>
-                  <!--------here related taxonomy can be desplayed------------->
+                <?php
+                $completion_time = get_post_meta(get_the_ID(), 'completion_time', true);
+                $area = get_post_meta(get_the_ID(), 'area', true);
+                
+                if (!empty($completion_time)) : ?>
+                  <p class="related-projects-time">
+                    <i class="fa fa-clock-o"></i>
+                    Сроки: <?php echo esc_html($completion_time); ?>
+                  </p>
+                <?php endif;
+                
+                if (!empty($area)) : ?>
+                  <p class="related-projects-area">
+                    <i class="fa fa-expand"></i>
+                    Площадь: <?php echo esc_html($area); ?> кв.м.
+                  </p>
                 <?php endif; ?>
               </div>
-              <p><a href="<?php the_permalink(); ?>" class="btn btn-primary related-projects-carousel-btn" role="button" title="<?php the_title(); ?>">Подробнее</a></p>
+              
+              <a href="<?php the_permalink(); ?>" class="related-projects-carousel-btn">
+                Подробнее
+              </a>
             </div>
           </div>
         </div>
+        <?php 
+        // Добавляем clearfix после каждого третьего элемента для MD
+        if($counter % 3 == 0) : ?>
+          <div class="clearfix visible-md visible-lg"></div>
+        <?php endif; ?>
+        <?php 
+        // Добавляем clearfix после каждого второго элемента для SM
+        if($counter % 2 == 0) : ?>
+          <div class="clearfix visible-sm"></div>
+        <?php endif; ?>
       <?php endwhile; else : ?>
         <p>Нет проектов для отображения.</p>
       <?php endif; ?>
